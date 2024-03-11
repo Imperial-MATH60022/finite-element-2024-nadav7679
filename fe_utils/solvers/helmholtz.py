c9d6f5b0409df47c0bdfdbe2cdf432ba914347cf
nadav7679 <nadav76799@gmail.com>
2024-03-11 09:56:06 +0000

    Working Helmholtzzzzzz!!!!

--- fe_utils/solvers/helmholtz.py
+++ fe_utils/solvers/helmholtz.py
@@ -15,18 +15,18 @@ from ..function_spaces import FunctionSpace, Function
 def assemble(fs: FunctionSpace, f: Function):
     """Assemble the finite element system for the Helmholtz problem given
     the function space in which to solve and the right hand side
-    function."""
+    function. Assume f.function_space and fs are on the same mesh."""
 
 
     # Create an appropriate (complete) quadrature rule.
-    quad = gauss_quadrature(fs.element.cell, fs.element.degree)
-    f_quad = gauss_quadrature(f.function_space.element.cell,f.function_space.element.degree)
+    quad = gauss_quadrature(fs.element.cell, fs.element.degree**2)
+    # f_quad = gauss_quadrature(f.function_space.element.cell,f.function_space.element.degree)
 
     # Tabulate the basis functions and their gradients at the quadrature points.
     local_phi = fs.element.tabulate(quad.points)
     local_phi_grad = fs.element.tabulate(quad.points, True)
 
-    local_psi = f.function_space.element.tabulate(f_quad.points)
+    # local_psi = f.function_space.element.tabulate(f_quad.points)
 
     # assemble RHS
     # This creates a sparse matrix because creating a dense one may
@@ -34,18 +34,29 @@ def assemble(fs: FunctionSpace, f: Function):
     A = sp.lil_matrix((fs.node_count, fs.node_count))
     l = np.zeros(fs.node_count)
 
-    # Now loop over all the cells and assemble A and l
-    print(local_phi.shape)
-    for c in range(f.function_space.mesh.entity_counts[-1]):
+    for c in range(fs.mesh.entity_counts[-1]):
+        #  local_phi_grad.shape = (q, i, a) where q num of quad points, i num of basis funcs, a dimensions
+        #  jac.shape = (d, a) where a==d (d is just another dummy var.)
+        #  local_phi.shape = (q, i)
+
+        jac = fs.mesh.jacobian(c)
+        jac_det = np.abs(np.linalg.det(jac))
+        jac_inv_t = np.linalg.inv(jac)
+
         c_nodes = f.function_space.cell_nodes[c, :]
-        cell_f = local_psi @ f.values[c_nodes].reshape((c_nodes.shape[0], 1))
 
-        cell_int = np.einsum("ij,kl,q->j", local_phi, cell_f, f_quad.weights)  # TODO: make sure its f_quad weights and not just quad
-        cell_int *= np.abs(np.linalg.det(fs.mesh.jacobian(c)))  # TODO: again, make sure this jacobian matches the quad rule (as there are two quad rules)
+        # RHS:
+        cell_f = local_phi @ f.values[c_nodes].reshape((c_nodes.shape[0], 1))
+        cell_f_int = np.einsum("qi,q,q->i", local_phi, np.squeeze(cell_f, 1), quad.weights)
 
-        l[fs.cell_nodes[c, :]] += cell_int
+        l[fs.cell_nodes[c, :]] += jac_det * cell_f_int
 
-    #TODO: Left Hand Side!!!!
+        # LFS:
+        jac_grad_phi = np.einsum("da,qid->aiq", jac_inv_t, local_phi_grad)
+        jac_grad_phi_squared = np.einsum("aiq,ajq->ijq", jac_grad_phi, jac_grad_phi)
+        term_2 = np.einsum("qi,qj->ijq", local_phi, local_phi)
+
+        A[np.ix_(c_nodes, c_nodes)] += jac_det * ((term_2 + jac_grad_phi_squared) @ quad.weights)
 
     return A, l
 
@@ -97,26 +108,26 @@ def solve_helmholtz(degree, resolution, analytic=False, return_error=False):
 
 
 if __name__ == "__main__":
-    # from argparse import ArgumentParser
-    #
-    # parser = ArgumentParser(
-    #     description="""Solve a Helmholtz problem on the unit square.""")
-    # parser.add_argument("--analytic", action="store_true",
-    #                     help="Plot the analytic solution instead of solving the finite element problem.")
-    # parser.add_argument("--error", action="store_true",
-    #                     help="Plot the error instead of the solution.")
-    # parser.add_argument("resolution", type=int, nargs=1,
-    #                     help="The number of cells in each direction on the mesh.")
-    # parser.add_argument("degree", type=int, nargs=1,
-    #                     help="The degree of the polynomial basis for the function space.")
-    # args = parser.parse_args()
-    # resolution = args.resolution[0]
-    # degree = args.degree[0]
-    # analytic = args.analytic
-    # plot_error = args.error
-    #
-    # u, error = solve_helmholtz(degree, resolution, analytic, plot_error)
+    from argparse import ArgumentParser
 
-    u, error = solve_helmholtz(2, 3, False, False)
+    parser = ArgumentParser(
+        description="""Solve a Helmholtz problem on the unit square.""")
+    parser.add_argument("--analytic", action="store_true",
+                        help="Plot the analytic solution instead of solving the finite element problem.")
+    parser.add_argument("--error", action="store_true",
+                        help="Plot the error instead of the solution.")
+    parser.add_argument("resolution", type=int, nargs=1,
+                        help="The number of cells in each direction on the mesh.")
+    parser.add_argument("degree", type=int, nargs=1,
+                        help="The degree of the polynomial basis for the function space.")
+    args = parser.parse_args()
+    resolution = args.resolution[0]
+    degree = args.degree[0]
+    analytic = args.analytic
+    plot_error = args.error
 
-    # u.plot()
+    u, error = solve_helmholtz(degree, resolution, analytic, plot_error)
+
+    # u, error = solve_helmholtz(2, 3, False, False)
+
+    u.plot()
